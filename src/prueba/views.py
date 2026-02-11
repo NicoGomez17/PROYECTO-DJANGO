eadd270b32655ef7e31a2ffc34513629e44081bb
Nicolas Gomez <nicogomez170401@gmail.com>
2026-02-11 20:44:06 -0300

    Creando paginas web

--- src/prueba/views.py
+++ src/prueba/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    datos = {
+        "titulo": "Django",
+        "descripcion": "Freamework para crear aplicaciones web",
+    }
+    return render(request, "prueba/index.html", datos)
+
+def notas(request):
+    mis_notas = [10, 7, 6, 3, 8, 4]
+    return render(request, "prueba/notas.html", {"notas": mis_notas})
